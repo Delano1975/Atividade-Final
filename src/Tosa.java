a699c15d6f7e9e37eafc4d9e76089c94425b79fd
Your Name~ <delanogadelha@hotmail.com>
2023-05-28 23:20:29 -0300

    Criacao da Classe Tosa

---
+++ src/Tosa.java
@@ -0,0 +1,22 @@
+public class Tosa {
+   
+    String simples;
+	String completa;
+	double precoSimples;
+	double precoCompleto;
+	
+	void tipoDeTosa () {
+		
+		System.out.println("Tosa simples: " + simples);
+		System.out.println("Tosa completa: " + completa);
+		
+	}
+	
+	void tipoDePreco () {
+
+		System.out.println("Preço simples: " + precoSimples);
+		System.out.println("Preço completo: " + precoCompleto);
+				
+	}
+} 
+
