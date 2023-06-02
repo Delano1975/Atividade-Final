76cf0c238a88f072c9a98b04a2201c4631dc5c05
Your Name~ <delanogadelha@hotmail.com>
2023-06-01 21:19:58 -0300

    Correção de novo método na Classe Cliente

---
+++ src/Cliente.java
@@ -0,0 +1,24 @@
+public class Cliente {
+    String nome;
+	String cpf;
+	String ativo;
+	String inativo;
+	
+	void dadosDoCliente () {
+
+		System.out.println("Nome do cliente: " + nome);
+		System.out.println("Cpf do clinete: " + cpf);
+		
+	}
+	
+	void statusDoCliente () {
+
+		System.out.println("Status do Cliente: " + ativo);
+		System.out.println("Status do Cliente: " + inativo);
+	}
+
+    void clienteAtivo () {
+
+		System.out.println("Status do Cliente: " + ativo);
+	}
+}	
