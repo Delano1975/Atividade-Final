f379ec5f54946cfda4a6737c891ec41c45829dfc
Maciel Cavalcante <134858575+MacielCN@users.noreply.github.com>
2023-06-01 21:53:47 -0300

    Create Banho.java

---
+++ src/Banho.java
@@ -0,0 +1,21 @@
+public class Banho {
+
+	String simples;
+	String completo;
+	double precoSimples;
+	double precoCompleto;
+	
+	void tipoDeBanho () {
+
+		System.out.println("Banho simples: " + simples);
+		System.out.println("Banho completo: " + completo);
+				
+	}
+
+	void tipoDePreco () {
+
+		System.out.println("Preço simples: " + precoSimples);
+		System.out.println("Preço completo: " + precoCompleto);
+				
+	}
+}
